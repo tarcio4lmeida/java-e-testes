ae8f6248707ae1aa4b34791184ab26104121759c
Tarcio Almeida <tarcioalmeidalima@gmail.com>
2021-05-03 19:55:14 -0300

    Praticando TDD

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -8,14 +8,10 @@ public class Leilao {
 
 	private String descricao;
 	private List<Lance> lances;
-	
+
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
-	}
-	
-	public void propoe(Lance lance) {
-		lances.add(lance);
 	}
 
 	public String getDescricao() {
@@ -26,6 +22,32 @@ public class Leilao {
 		return Collections.unmodifiableList(lances);
 	}
 
-	
-	
+	public void propoe(Lance lance) {
+		if (lances.isEmpty() || podeDarLance(lance.getUsuario())) {
+			lances.add(lance);
+		}
+	}
+
+	private boolean podeDarLance(Usuario usuario) {
+		return !ultimoLanceDado().getUsuario().equals(usuario) && qtdDelancesDo(usuario) < 5;
+	}
+
+	private int qtdDelancesDo(Usuario usuario) {
+		int total = 0;
+		for (Lance lance : lances) {
+			if (lance.getUsuario().equals(usuario))
+				total++;
+		}
+		return total;
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size() - 1);
+	}
+
+	public void dobraLance(Usuario steveJobs) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/test/TesteDoAvaliador.java
+++ src/test/TesteDoAvaliador.java
@@ -120,6 +120,55 @@ public class TesteDoAvaliador {
 		assertEquals(100.0, leiloeiro.getMenorLance(), 0.00001);
 	}
 	
+	@Test
+    public void naoDeveAceitarDoisLancesSeguidosDoMesmoUsuario() {
+        Leilao leilao = new Leilao("Macbook Pro 15");
+        Usuario steveJobs = new Usuario("Steve Jobs");
+
+        leilao.propoe(new Lance(steveJobs, 2000));
+        leilao.propoe(new Lance(steveJobs, 3000));
+
+        assertEquals(1, leilao.getLances().size());
+        assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
+    }
+	
+	  @Test
+	    public void naoDeveAceitarMaisDoQue5LancesDeUmMesmoUsuario() {
+	        Leilao leilao = new Leilao("Macbook Pro 15");
+	        Usuario steveJobs = new Usuario("Steve Jobs");
+	        Usuario billGates = new Usuario("Bill Gates");
+
+	        leilao.propoe(new Lance(steveJobs, 2000));
+	        leilao.propoe(new Lance(billGates, 3000));
+	        leilao.propoe(new Lance(steveJobs, 3000));
+	        leilao.propoe(new Lance(billGates, 3000));
+	        leilao.propoe(new Lance(steveJobs, 4000));
+	        leilao.propoe(new Lance(billGates, 3000));
+	        leilao.propoe(new Lance(steveJobs, 5000));
+	        leilao.propoe(new Lance(billGates, 3000));
+	        leilao.propoe(new Lance(steveJobs, 6000));
+	        leilao.propoe(new Lance(billGates, 999));
+	        leilao.propoe(new Lance(steveJobs, 7000));
+
+	        assertEquals(10, leilao.getLances().size());
+	        int ultimo = leilao.getLances().size() - 1;
+	        assertEquals(999, leilao.getLances().get(ultimo).getValor(), 0.00001);
+	    }
+	 
+	  @Test
+	  public void deveDobrarOUltimoLanceDado() {
+		  Leilao leilao = new Leilao("Macbook Pro 15");
+	        Usuario steveJobs = new Usuario("Steve Jobs");
+	        Usuario billGates = new Usuario("Bill Gates");
+	        
+	        leilao.propoe(new Lance(steveJobs, 2000));
+	        leilao.propoe(new Lance(billGates, 3000));
+	        leilao.dobraLance(steveJobs);
+	       
+	        assertEquals(4000, leilao.getLances().get(2).getValor(), 0.00001);
+	        
+	  }
+	
 }
 
 //Testes automatizados de unidade nos trazem diversas vantagens.
